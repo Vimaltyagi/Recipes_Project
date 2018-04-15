0b4f65935b99be88244b556f42223eadbae653c8
Vimaltyagi <vimaltyagi58@gmail.com>
2018-04-16 00:41:22 +0530

    Added if click on recipe item thens how the detail of the same item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,13 +8,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-recipes: Recipe[] = [
-  new Recipe('A Test Recipe','A is simply a test','http://www.sailusfood.com/wp-content/uploads/2016/02/cheese-corn-balls-recipe.jpg'),
-  new Recipe('A Test Recipe','A is simply a test','http://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/4/7/0/TM1C48_super-nachos_s4x3.jpg.rend.hgtvcom.966.725.suffix/1382539863868.jpeg')
-];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe 1', 'A is simply a test 1', 'http://www.sailusfood.com/wp-content/uploads/2016/02/cheese-corn-balls-recipe.jpg'),
+    new Recipe('A Test Recipe 2', 'A is simply a test 2', 'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/4/7/0/TM1C48_super-nachos_s4x3.jpg.rend.hgtvcom.966.725.suffix/1382539863868.jpeg')
+  ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
